da238fe63e1ea33e32155efeaa5d20480376dd93
fendysaputro <vidis194@gmail.com>
2019-09-11 20:48:50 +0700

    update

--- app/routers/customer.js
+++ app/routers/customer.js
@@ -15,6 +15,30 @@ router.get('/', function(req, res, next){
         if(error){
             console.log("An error happened -&gt; " + JSON.stringify(error));
             return res.json({r:false, m:error})
+        } 
+
+        res.json({r:true, m: "Success", d: vendors})
+    });
+});
+
+router.post('/authenticate', function(req, res, next){
+    CustomerModel.find({email: req.body.email, password: req.body.password}, function(error, customers){
+        if(error){
+            console.log("An error happened -&gt; " + JSON.stringify(error));
+            return res.json({r:false, m:error})
+        }
+        if (customers.length == 0){
+            return res.json({r:false, m:"User or Password not found"});
+        }
+
+        var i = customers[0].name;
+        var s = customers[0].email;
+        var a = 'http://localhost:8002';
+
+        var option = {
+            issuer: i,
+            subject: s, 
+            audience: a,
         }
     })
 })
